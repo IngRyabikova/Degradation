4a6b402c636f4d2ce974c89b1e67aa17e05f42f2
ussh77 <70959657+ussh77@users.noreply.github.com>
2020-09-08 17:15:46 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+
+
+#include "TXLib.h"
+
+int main()
+    {
+    txCreateWindow (800, 600);
+
+    txSetColor (TX_WHITE);
+    txSetFillColor (TX_TRANSPARENT);
+    txRectangle (50, 50, 300, 490);
+
+
+    return 0;
+    }
+
